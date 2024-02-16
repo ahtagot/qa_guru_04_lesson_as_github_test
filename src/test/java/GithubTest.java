ad53bc5b628fda99c83a6955297ff8b2f15809b5
astelmakh13 <stelmakh.al@gmail.com>
2024-02-16 01:59:38 -0600

    added code example check

--- src/test/java/GithubTest.java
+++ src/test/java/GithubTest.java
@@ -16,6 +16,7 @@ public class GithubTest {
         Configuration.baseUrl = "https://github.com";
         Configuration.pageLoadStrategy = "eager";
 
+
     }
 
     @Test
@@ -33,7 +34,18 @@ public class GithubTest {
 
         // - Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
         $(byId("wiki-pages-box")).$(byText("SoftAssertions")).click();
-        $(byId("wiki-content")).shouldHave(text("3. Using JUnit5 extend test class:"));
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:\n" +
+                "@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
 
     }
 
